1d287f587e842595f7df62fd78c2891575d0c29f
jerbij <64164963+jerbij@users.noreply.github.com>
2022-05-12 22:13:17 +0200

    2.1 implemented, 2.3 implemented

---
+++ script.js
@@ -0,0 +1,47 @@
+function makeObjectDeepCopy(source){
+    if (typeof source !== 'object' || source === null) {
+        return source;
+    }
+
+    let destination;
+
+    if (Array.isArray(source)) {
+        destination = [];
+    } else {
+        destination = {};
+    }
+
+    for (let key in source) {
+        const currentElement = source[key];
+        destination[key] = makeObjectDeepCopy(currentElement);
+    }
+
+    return destination;
+}
+
+const myObject = {
+    from: 1,
+    to: 4,
+
+    [Symbol.iterator]: function () {
+
+        if ( ( this.from === undefined || this.to === undefined)
+            || (isNaN(this.from) || isNaN(this.to) )
+            || (this.to < this.from)
+        ) {
+            // lets show error
+            throw Error("from or to is invalid");
+        } else {
+            let self = this;
+            return {
+                current: self.from,
+                next: function () {
+                    return {
+                        done: !(this.current <= self.to),
+                        value: this.current++
+                    }
+                }
+            }
+        }
+    }
+}
